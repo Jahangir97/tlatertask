175ce0d3f57b2423b63f5eb26ea80e9a29c72695
Jahanda97 <jahangirjn@code.edu.az>
2022-04-20 09:33:22 +0400

    first commit

---
+++ task22/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Array3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir eded daxil edin:");
+            int numm = Convert.ToInt32(Console.ReadLine());
+            int number;
+            if (numm < 50)
+            {
+                Console.WriteLine("\n3-e boluneededler:");
+                int divLen = numm / 3 + 1;
+                int[] divisibles3 = new int[divLen];
+                for (number = 0; number < numm; number++)
+                {
+                    int index = number / 3;
+                    if (number % 3 == 0)
+                    {
+                        divisibles3[index] = number;
+                        Console.Write(divisibles3[index] + " ");
+                    }
+                }
+            }
+
+            else if (numm > 100)
+            {
+                Console.WriteLine("\n8-e bolun ededler:");
+                int divLen = numm / 8 + 1;
+                int[] divisibles8 = new int[divLen];
+                for (number = 0; number < numm; number++)
+                {
+                    int index = number / 8;
+                    if (number % 8 == 0)
+                    {
+                        divisibles8[index] = number;
+                        Console.Write(divisibles8[index] + " ");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("\n5-e bolun ededler:");
+                int divLen = numm / 5 + 1;
+                int[] divisibles5 = new int[divLen];
+                for (number = 0; number < numm; number++)
+                {
+                    if (number % 5 == 0)
+                    {
+                        int index = number / 5;
+                        divisibles5[index] = number;
+                        Console.Write(divisibles5[index] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ taskk1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Mod7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" eded daxil edin: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int mod = number % 7;
+            int result;
+            if (mod > 3)
+            {
+                result = number + 7 - mod;
+                Console.WriteLine(result);
+            }
+            else if (mod == 0)
+            {
+                Console.WriteLine("Eded yeddiye qaliqsiz bolunur.");
+            }
+            else
+            {
+                result = number - mod;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
+
